6dcee846a74bdee400877e49f496e4daf98fd921
tetsuro_sano <tetsuro_sano@dwango.co.jp>
2017-04-17 06:37:57 +0000

    Make bin folder, host & port configurable.

--- packages/cycle-scripts-one-fits-all/scripts/start.js
+++ packages/cycle-scripts-one-fits-all/scripts/start.js
@@ -2,9 +2,13 @@
 const spawn = require('cross-spawn');
 const path = require('path');
 
+require('dotenv').config({silent: true});
 let env = Object.create( process.env );
 env.NODE_ENV = 'development';
 
-const webpack = path.resolve(__dirname, '..', '..', '.bin', 'webpack-dev-server');
+let bin = env.BIN || path.join(__dirname, '..', '..', '.bin');
+const webpack = path.resolve(bin, 'webpack-dev-server');
 
-spawn.sync(webpack, ['--config', path.join(__dirname, 'configs', 'webpack.config.js')], { env: env, stdio: 'inherit' });
+let host = env.HOST || 'localhost';
+let port = env.PORT || '3000';
+spawn.sync(webpack, ['--config', path.join(__dirname, 'configs', 'webpack.config.js'), '--host', host, '--port', port], { env: env, stdio: 'inherit' });
